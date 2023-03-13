8692c2f60811fe52289226033ae2fa4a72c706b7
Uberto Barbini <uberto.gama@gmail.com>
2023-03-13 16:04:33 +0900

    added kleisi arrows

--- src/main/kotlin/com/gamasoft/kondor/mongo/core/MongoReader.kt
+++ src/main/kotlin/com/gamasoft/kondor/mongo/core/MongoReader.kt
@@ -6,14 +6,20 @@ import com.ubertob.kondor.outcome.Outcome
 typealias MongoReader<T> = ContextReader<MongoSession, T>
 typealias MongoOutcome<T> = Outcome<MongoError, T>
 
-fun <U, T> mongoCalculation(operation: MongoSession.(U) -> T): (U) -> ContextReader<MongoSession, T> =
+fun <U, T> mongoCalculation(operation: MongoSession.(U) -> T): (U) -> MongoReader<T> = //unit
     { input: U -> MongoReader { session -> operation(session, input) } }
 
-fun <T> mongoAction(operation: MongoSession.() -> T): ContextReader<MongoSession, T> =
+fun <T> mongoAction(operation: MongoSession.() -> T): MongoReader<T> =
     mongoCalculation<Unit, T>{ operation(this) }(Unit)
 
-fun <T, U> MongoReader<T>.bindCalculation(operation: MongoSession.(T) -> U): ContextReader<MongoSession, U> =
+fun <T, U> MongoReader<T>.bindCalculation(operation: MongoSession.(T) -> U): MongoReader<U> =
     bind { input -> mongoCalculation(operation)(input) }
+
+infix fun <T, U> MongoReader<T>.combineWith(operation: (T) -> MongoReader<U>): MongoReader<U> =
+    bind { input -> operation(input) }
+
+operator fun  <T, U> MongoReader<T>.plus(operation: (T) -> MongoReader<U>): MongoReader<U> =
+    combineWith(operation)
 
 infix fun <T : Any> MongoReader<T>.runOn(provider: MongoProvider): MongoOutcome<T> = provider.tryRun(this)
 
