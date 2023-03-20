955b7b429978ead18ce88d2ac488bf4a3e922af5
Uberto Barbini <uberto.gama@gmail.com>
2023-03-20 19:38:36 +0900

    added new methods in MongoSession

--- src/main/kotlin/com/gamasoft/kondor/mongo/core/MongoSession.kt
+++ src/main/kotlin/com/gamasoft/kondor/mongo/core/MongoSession.kt
@@ -2,31 +2,38 @@ package com.gamasoft.kondor.mongo.core
 
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.Filters
+import com.mongodb.client.model.FindOneAndUpdateOptions
+import com.mongodb.client.model.ReturnDocument
 import org.bson.BsonDocument
-import org.bson.BsonObjectId
+import org.bson.BsonValue
 import org.bson.conversions.Bson
 import java.util.concurrent.atomic.AtomicReference
 
 interface MongoSession {
 
     //Edit Methods
-    fun <T : Any> MongoTable<T>.addDocument(doc: T): BsonObjectId =
+    fun <T : Any> MongoTable<T>.addDocument(doc: T): BsonValue =
         addDocuments(listOf(doc)).first()
 
-    fun <T : Any> MongoTable<T>.all(): Sequence<T> = find("")
-
     //    fun <T : Any> MongoCollection<T>.replaceDocument(doc: BsonDocument): BsonObjectId
-    fun <T : Any> MongoTable<T>.addDocuments(docs: List<T>): Collection<BsonObjectId>
-    fun <T : Any> MongoTable<T>.removeDocuments(queryString: String): Long
+    fun <T : Any> MongoTable<T>.addDocuments(docs: Iterable<T>): List<BsonValue>
 
+    fun <T : Any> MongoTable<T>.removeDocuments(queryString: String): Long
+    fun <T : Any> MongoTable<T>.removeDocuments(bsonFilters: Bson): Long
+    fun <T : Any> MongoTable<T>.updateSingle(bsonFilters: Bson, bsonSetter: Bson): T?
 
     //Query Methods
+    fun <T : Any> MongoTable<T>.findById(id: Any): T?
     fun <T : Any> MongoTable<T>.find(queryString: String): Sequence<T>
+    fun <T : Any> MongoTable<T>.find(bsonFilters: Bson): Sequence<T>
+
+    fun <T : Any> MongoTable<T>.all(): Sequence<T> = find("")
     fun <T : Any> MongoTable<T>.aggregate(vararg pipeline: Bson): Sequence<BsonDocument>
 
     fun MongoTable<*>.countDocuments(): Long
 
-    //Other Methods
+    //Table Methods
     fun <T : Any> MongoTable<T>.drop()
 
     fun <T : Any> MongoTable<T>.listIndexes(): Sequence<BsonDocument>
@@ -46,23 +53,39 @@ class MongoDbSession(val database: MongoDatabase) : MongoSession {
         }
 
 
-    fun <T : Any> MongoTable<*>.internalRun(block: (MongoCollection<BsonDocument>) -> T): T =
+    fun <T> MongoTable<*>.internalRun(block: (MongoCollection<BsonDocument>) -> T): T =
         block(
             withCollection(this)
 
         )
 
-    override fun <T : Any> MongoTable<T>.addDocuments(docs: List<T>): Collection<BsonObjectId> =
+    override fun <T : Any> MongoTable<T>.addDocuments(docs: Iterable<T>): List<BsonValue> =
         internalRun {
-            val values = it.insertMany(docs.map(this::toBsonDoc))
-                .insertedIds.values
-            values.map { objId -> objId as BsonObjectId }
+            it.insertMany(docs.map(this::toBsonDoc))
+                .insertedIds.values.toList()
+        }
+
+    override fun <T : Any> MongoTable<T>.removeDocuments(bsonFilters: Bson): Long =
+        internalRun {
+            it.deleteMany(bsonFilters)
+                .deletedCount
         }
 
     override fun <T : Any> MongoTable<T>.removeDocuments(queryString: String): Long =
-        internalRun {
-            it.deleteMany(BsonDocument.parse(queryString))
-                .deletedCount
+        removeDocuments(BsonDocument.parse(queryString))
+
+    private val RETURN_UPDATED = FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER)
+    override fun <T : Any> MongoTable<T>.updateSingle(bsonFilters: Bson, bsonSetter: Bson): T? =
+        internalRun { coll ->
+            coll.findOneAndUpdate(bsonFilters, bsonSetter, RETURN_UPDATED)
+                ?.let ( ::fromBsonDoc )
+        }
+
+    override fun <T : Any> MongoTable<T>.findById(id: Any): T? =
+        internalRun { coll ->
+            coll.find(Filters.eq("_id", id))
+                .firstOrNull()
+                ?.let(::fromBsonDoc)
         }
 
     override fun <T : Any> MongoTable<T>.find(queryString: String): Sequence<T> =
@@ -71,7 +94,13 @@ class MongoDbSession(val database: MongoDatabase) : MongoSession {
                 "" -> it.find()
                 else ->
                     it.find(BsonDocument.parse(queryString))
-            }.asSequence().map { this.fromBsonDoc(it) }
+            }.asSequence().map(::fromBsonDoc)
+        }
+
+    override fun <T : Any> MongoTable<T>.find(bsonFilters: Bson): Sequence<T> =
+        internalRun {
+            it.find(bsonFilters)
+            .asSequence().map(::fromBsonDoc)
         }
 
     override fun <T : Any> MongoTable<T>.aggregate(vararg pipeline: Bson): Sequence<BsonDocument> =
